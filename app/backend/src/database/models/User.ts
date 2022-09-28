5822da66654883d787f3cd2a4057a6edfe42f742
Amanda Soares <amandchen@hotmail.com>
2022-09-28 10:07:44 -0300

    Models: creates model for users

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,41 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
+
+export default User;
