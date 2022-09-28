d7be631a0954770c3b074f9d66515fd0b28ee2d7
Amanda Soares <amandchen@hotmail.com>
2022-09-28 16:25:51 -0300

    Middlewares: Creates Custom middleware error

---
+++ app/backend/src/database/middlewares/jwt.ts
@@ -0,0 +1,22 @@
+import { sign, SignOptions, verify, JwtPayload } from 'jsonwebtoken';
+
+const SECRET = process.env.JWT_SECRET || 'g0t4s3cr3tc4ny0uk33p1t';
+const jwtOptions: SignOptions = { expiresIn: '4d', algorithm: 'HS256' };
+
+const generateToken = async (email: string): Promise<string> => {
+  const token = sign({ email }, SECRET, jwtOptions);
+  // console.log(token);
+
+  return token;
+};
+
+const verifyToken = (token: string) => {
+  try {
+    const decoded = verify(token, SECRET) as JwtPayload;
+    return decoded;
+  } catch (err) {
+    return err;
+  }
+};
+
+export { generateToken, verifyToken };
