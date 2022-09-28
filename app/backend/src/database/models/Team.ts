6c75f87d596d26716d765450131a2ea1592a3b49
Amanda Soares <amandchen@hotmail.com>
2022-09-28 10:08:03 -0300

    Models: creates model for teams

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,29 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  public id!: number;
+  public name!: string;
+  public description!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'Team',
+  tableName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
