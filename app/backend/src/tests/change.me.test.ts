850d0bba85c38585124d8d2463471d31dacc081e
Amanda Soares <amandchen@hotmail.com>
2022-09-28 22:20:59 -0300

    Tests: Creates tests for route GET /login/validate

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -6,7 +6,7 @@ import chaiHttp = require('chai-http');
 import User from '../database/models/User';
 import { app } from '../app';
 import { Response } from 'superagent';
-import userMock from './mocks';
+import { userMock, userMock2, mockToken } from './mocks';
 
 
 chai.use(chaiHttp);
@@ -47,6 +47,16 @@ describe('Testa a rota POST /login', () => {
     expect(chaiHttpResponse.body).to.have.equal({ message: 'Incorrect email or password' });
   });
 
+  it('verifica se o usuário não é logado o usuário não existe', async () => {
+    chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: "not_exists@email.com", password: userMock.password });
+    
+    expect(chaiHttpResponse.status).to.equal(400);
+    expect(chaiHttpResponse.body).to.have.equal({ message: 'Incorrect email or password' });
+  });
+
   it('verifica se o usuário não enviar o email a requisição falha', async () => {
     chaiHttpResponse = await chai
         .request(app)
@@ -55,5 +65,51 @@ describe('Testa a rota POST /login', () => {
     
     expect(chaiHttpResponse.status).to.equal(400);
     expect(chaiHttpResponse.body).to.have.equal({ message: 'All fields must be filled' });
+  });
+});
+
+describe('Testa a rota GET /login/validate', () => {
+  let chaiHttpResponse: Response;
+
+  beforeEach(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({
+        ...userMock2,
+      } as User);
+  });
+
+  afterEach(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('verifica se o usuário foi validado com sucesso', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/login/validate')
+       .set('authorization', mockToken);
+
+    expect(chaiHttpResponse.status).to.equal(200);
+    expect(chaiHttpResponse.body).to.have.equal({ role: userMock2.role });
+  });
+
+  it('verifica se o usuário foi não é validado se o token é inválido', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/login/validate')
+       .set('authorization', "invalid_token");
+
+    expect(chaiHttpResponse.status).to.equal(401);
+    expect(chaiHttpResponse.body).to.have.equal({ message: 'Invalid token' });
+  });
+
+  it('verifica se o usuário foi não é validado se o token não é fornecido', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/login/validate')
+       .set('authorization', "");
+
+    expect(chaiHttpResponse.status).to.equal(401);
+    expect(chaiHttpResponse.body).to.have.equal({ message: 'No token was informed' });
   });
 });
--- app/backend/src/tests/mocks/index.ts
+++ app/backend/src/tests/mocks/index.ts
@@ -6,4 +6,14 @@ const userMock = {
   password: 'g4t1nh0sc4tsf0r3v3r',
 };
 
-export default userMock;
+const userMock2 = {
+  id: 2,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: 'secret_admin',
+};
+
+const mockToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInVzZXJuYW1lIjoiQWRtaW4iLCJpYXQiOjE2NjQzOTA5NDgsImV4cCI6MTY2NDczNjU0OH0.qLfEknMRmjSxeYvv-DHbYd9M-kjapIMyTby296wW8KU"
+
+export { userMock, userMock2, mockToken };
