295fc0a20d5d53e71b9a8c4977cc42bb6b85ba46
Amanda Soares <amandchen@hotmail.com>
2022-09-29 11:05:50 -0300

    MatchController: refactors controller to delete unused method

--- app/backend/src/database/controllers/matchController.ts
+++ app/backend/src/database/controllers/matchController.ts
@@ -2,11 +2,6 @@ import { Request, Response } from 'express';
 import MatchService from '../services/matchService';
 
 class MatchController {
-  // static getMatches = async (_req: Request, res: Response) => {
-  //   const matches = await MatchService.getMatches();
-  //   return res.status(200).json(matches);
-  // };
-
   static getMatches = async (req: Request, res: Response) => {
     const { inProgress } = req.query;
     if (!inProgress) {
